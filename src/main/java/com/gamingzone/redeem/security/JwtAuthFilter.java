0e405b8121c10912648fd8273e0b089b1f29b9bc
zvirot1 <zvirot@gmail.com>
2023-11-25 20:32:51 +0200

    automatic no stay connect

--- src/main/java/com/gamingzone/redeem/security/JwtAuthFilter.java
+++ src/main/java/com/gamingzone/redeem/security/JwtAuthFilter.java
@@ -3,10 +3,13 @@ package com.gamingzone.redeem.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,7 +20,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -27,10 +32,30 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtAuthExtractor extractor;
     //@Autowired
     //private JwtUtil jwtUtil;
+   // private final List<String> PATHS_TO_IGNORE_SETTING_SAMESITE = Arrays.asList("resources", <add other paths you want to exclude>);
+    private final String SESSION_COOKIE_NAME = "JSESSIONID";
+    private final String SESSION_PATH_ATTRIBUTE = ";Path=";
+    private final String ROOT_CONTEXT = "/";
+    private final String SAME_SITE_ATTRIBUTE_VALUES = ";HttpOnly;Secure;SameSite=None";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-/*
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String requestUrl = req.getRequestURL().toString();
+        System.out.println("requestUtl: " + requestUrl );
+            Cookie[] cookies = ((HttpServletRequest) request).getCookies();
+            if (cookies != null && cookies.length > 0) {
+                List<Cookie> cookieList = Arrays.asList(cookies);
+                Cookie sessionCookie = cookieList.stream().filter(cookie -> SESSION_COOKIE_NAME.equals(cookie.getName())).findFirst().orElse(null);
+                if (sessionCookie != null) {
+                    String contextPath = request.getServletContext() != null && StringUtils.isNotBlank(request.getServletContext().getContextPath()) ? request.getServletContext().getContextPath() : ROOT_CONTEXT;
+                    resp.setHeader(HttpHeaders.SET_COOKIE, sessionCookie.getName() + "=" + sessionCookie.getValue() + SESSION_PATH_ATTRIBUTE + contextPath + SAME_SITE_ATTRIBUTE_VALUES);
+                }
+            }
+
+        /*
         String userName = null;
         boolean pass = true;
         Map<String, Object> jwtValues = null;
@@ -77,6 +102,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
            extractor.extract(request)
                    .ifPresent(SecurityContextHolder.getContext()::setAuthentication);
        }
+
+
         filterChain.doFilter(request, response);
     }
 }
